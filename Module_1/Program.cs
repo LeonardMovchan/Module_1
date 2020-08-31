ac8011e73f23054b2a56336efa23bbeb3c34bd41
Leonard Movchan <lanakrit@gmail.com>
2020-08-31 10:26:23 +0300

    Enum, Menu functional added

--- Module_1/Program.cs
+++ Module_1/Program.cs
@@ -10,6 +10,54 @@ namespace Module_1
     {
         static void Main(string[] args)
         {
+            do
+            {
+                Console.WriteLine("=======MENU=======");
+                Console.WriteLine("1. Add new employee");
+                Console.WriteLine("2. Check all employees");
+                Console.WriteLine("3. Remove employee");
+                Console.WriteLine("4. Exit");
+
+                Menu menu = (Menu)int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case Menu.AddNewEmployee:
+                        {
+
+                        }break;
+                    case Menu.CheckEmployees:
+                        {
+
+                        }break;
+                    case Menu.RemoveEmployee:
+                        {
+
+                        }break;
+                    case Menu.Exit:
+                    {
+
+                    }break;
+
+                    default:
+                        {
+                            Console.WriteLine("There is no such command");
+                        }break;
+                }
+
+            } while (true);
         }
+
+        enum Menu
+        {
+            NoItem = 0,
+            AddNewEmployee = 1,
+            CheckEmployees = 2,
+            RemoveEmployee = 3,
+            Exit = 4
+        }
+            
+
+        
     }
 }
